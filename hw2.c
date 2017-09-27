faa19bd81fdd73f2f1c617ff1ee9d2647175e593
Shane Moo <obries6@rpi.edu>
2017-09-26 23:49:43 -0400

    Basic cases complete. Fix all specific cases soon

--- hw2.c
+++ hw2.c
@@ -8,7 +8,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-int singleProcess(char* expression, char singleEx, int location) {
+int singleProcess(char* expression, char singleEx, int location, int* err) {
 	int p[2];
 
 	int rc = pipe(p);
@@ -56,8 +56,9 @@ int singleProcess(char* expression, char singleEx, int location) {
 	}
 }
 
-int parenProcess(char* fullEx, char* singleEx, int location) {
-	printf("inside process: %s\n", singleEx);
+int parenProcess(char* fullEx, char* singleEx, int location, int* err) {
+	int pid = getpid();
+	printf("PID %d: My expression is \"%s\"\n", pid, singleEx);
 	fflush(NULL);
 	// printf("location: %d\n", location);
 	// fflush(NULL);
@@ -70,42 +71,170 @@ int parenProcess(char* fullEx, char* singleEx, int location) {
 		cutParen[i] = singleEx[i+1];
 		i++;
 	}
+	int lengthEx = i;
 	cutParen[location] = '\0';
 
 	//printf("cut paren: %s\n", cutParen);
 	//fflush(NULL);
 
-	int lengthEx = i;
-	char newEx[lengthEx];
-	int tempResult = 0;
 	i = 0;
-	while (i < lengthEx) {
-		int con = cutParen[i];
-		if (con == 40) { //need to send whole parentheses
-			j = 0;
-			while(1) {
-				int con2 = cutParen[i+j];
-				newEx[j] = cutParen[i+j];
-				if (con2 == 41) { //end parentheses
-					break;
-				}
-				else{
-					j++;
-				}
-			}
-			//send expression here
-			tempResult += parenProcess(cutParen, newEx, j);
-		}
-		if (isdigit(cutParen[i])) {
-			tempResult += singleProcess(cutParen, cutParen[i], i);
+	while (i < strlen(cutParen)) {
+		if (isspace(cutParen[i])) {
+			break;
 		}
 		i++;
 	}
+	int convOp = 0;
+	if (i == 1) {
+		convOp = (int)cutParen[0];
+		if(convOp != 42 && convOp != 43 && convOp != 45 && convOp != 47) {
+			printf("PID %d: ERROR: unknown \"%c\" operator; exiting\n", pid, convOp);
+			fflush(NULL);
+			*err = 1;
+		}
+	}
+	else {
+		char wrongOp[128];
+		j = 0;
+		while (j < i) {
+			wrongOp[j] = cutParen[j];
+			j++;
+		}
+		printf("PID %d: ERROR: unknown \"%s\" operator; exiting\n", pid, wrongOp);
+		fflush(NULL);
+		*err = 1;
+	}
 
-	return tempResult;
+	if (*err == 0) {
+		printf("PID %d: Starting \"%c\" operation\n", pid, convOp);
+		fflush(NULL);
+	}
+
+	//now, to fork:
+	int p[2];
+
+	int rc = pipe(p);
+	if (rc == -1) {
+		perror("pipe() failed");
+	}
+
+	pid = fork();
+	if (pid == -1) {
+		perror("fork() failed");
+	}
+
+	if (pid == 0) {/*child*/
+		close(p[0]);
+		p[0] = -1;
+
+		int mypid = getpid();
+
+		//write expression here
+
+		char newEx[lengthEx];
+		int tempResult = 0;
+		i = 0;
+		while (i < lengthEx) {
+			int con = cutParen[i];
+			if (con == 40) { //need to send whole parentheses
+				j = 0;
+				while(1) {
+					int con2 = cutParen[i+j];
+					newEx[j] = cutParen[i+j];
+					if (con2 == 41) { //end parentheses
+						break;
+					}
+					else{
+						j++;
+					}
+				}
+				if (convOp == 43) {
+					tempResult += parenProcess(cutParen, newEx, j, err);
+				}
+				if (convOp == 47) {
+					tempResult /= parenProcess(cutParen, newEx, j, err);
+				}
+				if (convOp == 42) {
+					tempResult *= parenProcess(cutParen, newEx, j, err);
+				}
+				if (convOp == 45) {
+					tempResult -= parenProcess(cutParen, newEx, j, err);
+				}
+				
+			}
+			if (isdigit(cutParen[i])) {
+				if (convOp == 43) {
+					tempResult += singleProcess(cutParen, cutParen[i], i, err);
+				}
+				if (convOp == 47) {
+					if (i == 2) {
+						tempResult = singleProcess(cutParen, cutParen[i], i, err);
+					}
+					else {
+						if (cutParen[i] == '0') {
+							printf("PID %d: ERROR: division by zero is not allowed; exiting\n", mypid);
+							fflush(NULL);
+							*err = 1;
+						}
+						else {
+							tempResult /= singleProcess(cutParen, cutParen[i], i, err);
+						}
+					}
+				}
+				if (convOp == 42) {
+					if (i == 2) {
+						tempResult = singleProcess(cutParen, cutParen[i], i, err);
+					}
+					else {
+						tempResult *= singleProcess(cutParen, cutParen[i], i, err);
+					}
+				}
+				if (convOp == 45) {
+					if (i == 2) {
+						tempResult = singleProcess(cutParen, cutParen[i], i, err);
+					}
+					else {
+						tempResult -= singleProcess(cutParen, cutParen[i], i, err);
+					}
+				}
+			}
+			i++;
+		}
+
+		if (*err == 0) {
+			printf("PID %d: Processed \"(%s)\"; sending \"%d\" on pipe to parent\n", mypid, cutParen, tempResult);
+			char buff[128];
+			sprintf(buff, "%d", tempResult);
+
+			int bytes_written = write(p[1], buff, 128);
+
+			if (bytes_written == -1) {
+				perror("write() failed");
+			}
+		}
+		//say returning expression here, with result
+		_exit(0);
+	}
+
+	else { //parent
+		close(p[1]);
+		p[1] = -1;
+
+		char buffer[128];
+		int bytes_read = read(p[0], buffer, 128);
+
+		if (bytes_read == 0) {
+			*err = 1;
+		}
+
+		//convert to int and return
+		int toReturn = atoi(buffer);
+
+		return toReturn;
+	}
 }
 
-int calculator(char* expression) {
+int calculator(char* expression, int* err) {
 	int i = 0;
 	int j = 0;
 
@@ -134,10 +263,38 @@ int calculator(char* expression) {
 	cutParen[i+1] = '\0';
 
 	//get operator
+	i = 0;
+	while (i < strlen(cutParen)) {
+		if (isspace(cutParen[i])) {
+			break;
+		}
+		i++;
+	}
+	int convOp = 0;
+	if (i == 1) {
+		convOp = (int)cutParen[0];
+		if(convOp != 42 && convOp != 43 && convOp != 45 && convOp != 47) {
+			printf("PID %d: ERROR: unknown \"%c\" operator; exiting\n", pid, convOp);
+			fflush(NULL);
+			*err = 1;
+		}
+	}
+	else {
+		char wrongOp[128];
+		j = 0;
+		while (j < i) {
+			wrongOp[j] = cutParen[j];
+			j++;
+		}
+		printf("PID %d: ERROR: unknown \"%s\" operator; exiting\n", pid, wrongOp);
+		fflush(NULL);
+		*err = 1;
+	}
 
-	char operator = cutParen[0];
-	printf("PID %d: Starting \"%c\" operation\n", pid, operator);
-	fflush(NULL);
+	if (err == 0) {
+		printf("PID %d: Starting \"%c\" operation\n", pid, cutParen[0]);
+		fflush(NULL);
+	}
 
 	#if DEBUG_MODE
 		printf("cut paren: %s\n", cutParen);
@@ -145,8 +302,8 @@ int calculator(char* expression) {
 	#endif
 
 	i = 0;
-	char singleEx[20];
-	int result;
+	char singleEx[128];
+	int result = 0;
 	while(i < strlen(cutParen)) {
 		int con = cutParen[i];
 		if (con == 40) { //need to send whole parentheses
@@ -161,12 +318,55 @@ int calculator(char* expression) {
 					j++;
 				}
 			}
-			//send cutParen here
-			result += parenProcess(cutParen, singleEx, j);
+			if (convOp == 43) {
+				result += parenProcess(cutParen, singleEx, j, err);
+			}
+			if (convOp == 47) {
+				result /= parenProcess(cutParen, singleEx, j, err);
+			}
+			if (convOp == 42) {
+				result *= parenProcess(cutParen, singleEx, j, err);
+			}
+			if (convOp == 45) {
+				result -= parenProcess(cutParen, singleEx, j, err);
+			}
 			i += j;
 		}
 		if (isdigit(cutParen[i])) {
-			result += singleProcess(cutParen, cutParen[i], i);
+			if (convOp == 43) {
+				result += singleProcess(cutParen, cutParen[i], i, err);
+			}
+			if (convOp == 47) {
+				if (i == 2) {
+					result = singleProcess(cutParen, cutParen[i], i, err);
+				}
+				else {
+					if (cutParen[i] == 0) {
+						printf("PID %d: ERROR: division by zero is not allowed; exiting\n", pid);
+						fflush(NULL);
+						*err = 1;
+					}
+					else {
+						result /= singleProcess(cutParen, cutParen[i], i, err);
+					}
+				}
+			}
+			if (convOp == 42) {
+				if (i == 2) {
+					result = singleProcess(cutParen, cutParen[i], i, err);
+				}
+				else {
+					result *= singleProcess(cutParen, cutParen[i], i, err);
+				}
+			}
+			if (convOp == 45) {
+				if (i == 2) {
+					result = singleProcess(cutParen, cutParen[i], i, err);
+				}
+				else {
+					result -= singleProcess(cutParen, cutParen[i], i, err);
+				}
+			}
 		}
 		i++;
 	}
@@ -184,8 +384,20 @@ int main(int argc, char* argv[]) {
 		}
 	#endif
 
-	int answer = calculator(argv[1]);
-	printf("answer: %d\n", answer);
+	int err = 0;
+	int answer = 0;
+	answer = calculator(argv[1], &err);
+	int pid = getpid();
+
+	if (argc != 2) {
+		perror("ERROR: Invalid arguments\nUSAGE: ./a.out <input-file>");
+		return EXIT_FAILURE;
+	}
+
+	if (err == 0) {
+		printf("PID %d: Processed \"%s\"; final answer is \"%d\"\n", pid, argv[1], answer);
+		fflush(NULL);
+	}
 
 	return EXIT_SUCCESS;
 }
